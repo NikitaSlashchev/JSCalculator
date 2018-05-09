7cc2dc19b559fe81af69f6c58308714da60c69c7
NikitaSlashchev <restruct.man@gmail.com>
2018-05-09 22:57:47 +0300

    JSCalculator: More math stuff

--- JSCalculator/main.js
+++ JSCalculator/main.js
@@ -1,5 +1,5 @@
 while (exit != true) {
-	var inputMenu = +prompt('Calculator ^_^ \n 1:Addition \n 2:Subtraction \n 3:Division \n 4:Multiplication \n 5:Exponentiation', '');
+	var inputMenu = +prompt('Calculator ^_^ \n 1:Addition \n 2:Subtraction \n 3:Division \n 4:Multiplication \n 5:Exponentiation \n 6:Convert into binary \n 7:Sqare of number', '');
 	switch (inputMenu) {
 
 		case 1:
@@ -25,20 +25,30 @@ while (exit != true) {
 			var b = +prompt('Type b', '');
 			alert(multiplyNumber(a, b));
 			break;
-			
+
 		case 5:
 			var x = +prompt('Type x', '');
 			var n = +prompt('Type n', '');
 			alert(expNumber(x, n));
 			break;
 
+		case 6:
+			var a = +prompt('Type a', '');
+			alert(convertBinary(a));
+			break;
+
+		case 7:
+			var a = +prompt('Type a', '');
+			alert(sqareNumber(a));
+			break;
+
 		default:
 
-			var exit = confirm('Want to get out?');	
+			var exit = confirm('Want to get out?');
 
 	}
 
-} 
+}
 
 
 function additionNumber(a, b) {
@@ -61,7 +71,17 @@ function multiplyNumber(a, b) {
 	return a * b;
 }
 
-function expNumber(x, n){
-	
+function expNumber(x, n) {
+
 	return Math.pow(x, n);
+}
+
+function convertBinary(a) {
+
+	return a.toString(2);
+}
+
+function sqareNumber(a) {
+
+	return Math.sqrt(a);
 }
